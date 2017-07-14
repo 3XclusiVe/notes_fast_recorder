708d69bc39308e452f703c5a95cfb92164bc9870
Дмитрий <malllkin007@gmail.com>
2017-07-14 16:31:14 +0300

    Добавлена задержка нажатия.

--- audio_notes_recorder.py
+++ audio_notes_recorder.py
@@ -1,6 +1,8 @@
 # coding=utf-8
+import threading
+
 import pyaudio
-from datetime import datetime
+from datetime import datetime, time
 import os
 
 # coding=utf-8
@@ -14,14 +16,19 @@ current_audio_file = []
 def start_recording():
     global is_recording, current_audio_file
     is_recording = True;
-    print ("Sound")
-    os.system('\a')
     time_stamp = datetime.now()
     filename = time_stamp.strftime('%Y_%m_%d_%H_%M_%S') + ".wav"
     print ("Start recording")
+
     rec = Recorder(channels=2)
     current_audio_file = rec.open(filename, 'wb')
     current_audio_file.start_recording()
+    threading.Timer(1, sound).start()
+
+
+def sound():
+    os.system('\a')
+    print ("Sound")
 
 
 def end_recording():
@@ -32,6 +39,7 @@ def end_recording():
     print ("Stop recording")
     current_audio_file.stop_recording()
     current_audio_file.close();
+
 
 def user_handler():
     if (is_recording):
